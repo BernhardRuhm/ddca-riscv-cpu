6c305f56789ee531b54b2be10b7a04ed8a914297
bernhard <berni.ruhm@gmail.com>
2023-05-19 16:35:33 +0200

    began to write mem_access process

--- miriv/vhdl/memu.vhd
+++ miriv/vhdl/memu.vhd
@@ -25,8 +25,66 @@ entity memu is
 end entity;
 
 architecture rtl of memu is
+
 begin
 	mem_access : process(all)
+	begin
+		R  <= (ohters => '0');
+		B  <= '0';
+		XL <= '0';
+		XS <= '0';
+		M  <= MEM_OUT_NOP;
+
+		-- load or store exception
+		if (((op.memtype = MEM_H or op.memtype = MEM_HU or op.memtype = MEM_W) 
+				and (A(1 downto 0) = "01" or A(1 downto 0) = "11")) 
+			or (op.memtype = MEM-W and A(1 downto 0) = "10")) then 		
+				
+				XS <= '1' when op.memwrite = '1' else '0';
+				XL <= '1' when op.memread  = '1' else '0';
+		else
+
+			M.adress <= A;
+			M.rd <= op.memread;
+			M.wr <= op.memwrite;
+			B <= D.busy;	
+
+		-- write access
+			if (op.memwrite = '1') then 
+				if (op.memtype = MEM_W) then
+					M.byteena <= "1111";
+					M.wrdata  <= W(7 downto 0) & W(15 downto 8) & W(23 downto 16) & W(31 downto 24);
+				elsif (op.memtype = MEM_H or op.memtype = MEM_HU) then
+					if (A(1 downto 0) = "00" or A(1 downto 0) = "01") then
+						M.byteena <= "1100";
+						M.wrdata  <= ((31 downto 16) => (W(7 downto 0) & W(15 downto 8)),  others => '-');
+					elsif (A(1 downto 0) = "10" or A(1 downto 0) = "11") then
+						M.byteena <= "0011";
+						M.wrdata  <= ((23 downto 0) => (W(7 downto 0) & W(15 downto 8)),  others => '-');
+					end if;
+				elsif (op.memtype = MEM_B or op.memtype = MEM_BU) then
+					if (A(1 downto 0) = "00") then
+						M.byteena <= "1000";
+						M.wrdata  <= ((31 downto 24) => W(7 downto 0), others => '-');
+					elsif (A(1 downto 0) = "01") then
+						M.byteena <= "0100";
+						M.wrdata  <= ((23 downto 16) => W(7 downto 0), others => '-');
+					elsif (A(1 downto 0) = "10") then
+						M.byteena <= "0010";
+						M.wrdata  <= ((15 downto 8) => W(7 downto 0), others => '-');
+					elsif (A(1 downto 0) = "11") then
+						M.byteena <= "0001";
+						M.wrdata  <= ((7 downto 0) => W(7 downto 0), others => '-');
+					end if;
+				end if;
+			end if;
+
+		-- read access
+			if (op.memread = '1') then
+
+			end if;
+		end if;
+			
 	begin
 	end process;	
 end architecture;
